c84f7f8e0f7b96db0dc72ebe85e7f8eefa937793
ailingangel <angel20081018@163.com>
2018-10-13 23:03:03 +0800

    修改input组件的样式--通过将默认的type=file input元素设置成display:none然后点击另一个元素调用这个input元素的click事件即可

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="app">
-        <Upload multiple show-upload-list/>
+        <Upload multiple show-upload-list>
+            <Button>上传</Button>
+        </Upload>
     </div>
 </template>
 <script>
--- src/components/Upload/Upload.vue
+++ src/components/Upload/Upload.vue
@@ -1,6 +1,9 @@
 <template>
     <div :class="cls">
-        <input ref="file" type="file" :name="name" :class="uploadBtncls" :multiple="multiple" :accept="accept" @change="uploadChange"/>
+        <div :class="uploadBtncls" @click="handleClick">
+            <input ref="file" type="file" :name="name" :multiple="multiple" :accept="accept" @change="uploadChange" />
+            <slot></slot>
+        </div>
         <UploadList :files="fileList" v-if="showUploadList"></UploadList>
     </div>
 </template>
@@ -8,42 +11,45 @@
 const prefixCls = 'v-upload';
 import UploadList from './UploadList';
 export default {
-	components: {
-		UploadList
-	},
-	props: {
-		name: {
-			type: String,
-			default: 'file'
-		},
-		multiple: {
-			type: Boolean,
-			default: false
-		},
-		accept: String,
-		showUploadList: {
-			type: Boolean,
-			default: true
-		}
-	},
-	data() {
-		return {
-			fileList: undefined
-		}
-	},
+    components: {
+        UploadList
+    },
+    props: {
+        name: {
+            type: String,
+            default: 'file'
+        },
+        multiple: {
+            type: Boolean,
+            default: false
+        },
+        accept: String,
+        showUploadList: {
+            type: Boolean,
+            default: true
+        }
+    },
+    data() {
+        return {
+            fileList: undefined
+        }
+    },
     computed: {
         uploadBtncls() {
             return [`${prefixCls}-btn`];
         },
         cls() {
-        	return [`${prefixCls}-wraper`];
+            return [`${prefixCls}-wraper`];
         }
     },
     methods: {
-    	uploadChange(evt) {
-    		let selectedFiles = this.$refs.file.files; // 获取用户选中的文件
-    		this.fileList = selectedFiles;
-    	}
+        uploadChange(evt) {
+            let selectedFiles = this.$refs.file.files; // 获取用户选中的文件
+            this.fileList = selectedFiles;
+        },
+        handleClick() {
+        	this.$refs.file.click();
+        }
     }
 }
 </script>
